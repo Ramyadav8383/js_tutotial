333d253a6103ef03e46041c8bf01ea9c62ce4859
Ram Narayan Yadav <yram08913@gmail.com>
2022-12-16 13:49:03 +0530

    new commit

--- Js Tute/app.js
+++ Js Tute/app.js
@@ -627,9 +627,43 @@
 
 // when assigning no-primitive data type value to a variable is done by reference so any changes will affect all the references.
 
-const number = 1;
-let number2 = number;
-number2 = 7;
+// const number = 1;
+// let number2 = number;
+// number2 = 7;
 
-console.log(`the first value is ${number} `);
-console.log(`the second value is ${number2} `);
+// console.log(`the first value is ${number} `);
+// console.log(`the second value is ${number2} `);
+
+// let person = { name: 'bob' };
+// // let person2 = person;
+// let person2 = { ...person };
+
+// person2.name = 'susy';
+
+// console.log(`the name of the first person is ${person.name}`);
+// console.log(`the name of the second person is ${person2.name}`);
+
+// 31_null and undefined
+// both represent "no value"
+// undefined - "javascript can not find value for this"
+
+// variable without value
+// missing function arguments
+// missing object properties
+
+// null = "developer sets the value"
+// number = 20 + null; // 20+ 0
+// number2 = 30 + undefined;
+// console.log(number);
+// console.log(number2);
+
+// 32_truthy falsy
+// "", '', ``, 0, -0 , NaN, false , null, undefined
+const bool1 = true;
+const bool2 = 3 > 2;
+if (bool1) {
+  console.log("hey it works!");
+}
+if (bool2) {
+  console.log("hey it also works!");
+}
