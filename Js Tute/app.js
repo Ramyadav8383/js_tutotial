a4492e4ae7688da0af860c09bfabce4b574a7b2f
Ram Narayan Yadav <yram08913@gmail.com>
2023-01-02 13:09:49 +0530

    new commit

--- Js Tute/app.js
+++ Js Tute/app.js
@@ -949,7 +949,10 @@ const people = [
 ];
 
 const total = people.reduce(function (acc, currItem) {
-  console.log(acc);
-  console.log(currItem);
+  console.log(`total ${acc}`);
+  console.log(`current money: ${currItem.salary}`);
+  acc += currItem.salary;
   return acc;
-}, 200);
+}, 500);
+
+console.log(total);
---
+++ Js Tute/challenges/09_array.js
@@ -0,0 +1,39 @@
+// Array Methods #9
+
+// create students array
+const students = [
+  {
+    id: 1,
+    name: "peter",
+    score: 80,
+    favoriteSubject: "math",
+  },
+  {
+    id: 2,
+    name: "John",
+    score: 85,
+    favoriteSubject: "English",
+  },
+  {
+    id: 3,
+    name: "king",
+    score: 90,
+    favoriteSubject: "History",
+  },
+  {
+    id: 4,
+    name: "perry",
+    score: 70,
+    favoriteSubject: "social studies",
+  },
+  {
+    id: 5,
+    name: "parker",
+    score: 95,
+    favoriteSubject: "Hindi",
+  },
+];
+console.log(students);
+// const updatedStudents = students.map(function () {
+//   // console.log(updatedStudents);
+// });
