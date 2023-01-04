cf75d1226c2c74a295ad3dfd7078286f866dfcf4
Ram Narayan Yadav <yram08913@gmail.com>
2023-01-04 13:27:59 +0530

    new changes

--- Js Tute/challenges/09_array.js
+++ Js Tute/challenges/09_array.js
@@ -1,39 +1,66 @@
 // Array Methods #9
 
 // create students array
-const students = [
-  {
-    id: 1,
-    name: "peter",
-    score: 80,
-    favoriteSubject: "math",
-  },
-  {
-    id: 2,
-    name: "John",
-    score: 85,
-    favoriteSubject: "English",
-  },
-  {
-    id: 3,
-    name: "king",
-    score: 90,
-    favoriteSubject: "History",
-  },
-  {
-    id: 4,
-    name: "perry",
-    score: 70,
-    favoriteSubject: "social studies",
-  },
-  {
-    id: 5,
-    name: "parker",
-    score: 95,
-    favoriteSubject: "Hindi",
-  },
-];
-console.log(students);
+// console.log(students);
 // const updatedStudents = students.map(function () {
 //   // console.log(updatedStudents);
 // });
+
+// sayName("ram");
+
+// have acces to student from dat.js
+// map
+const updatedStudents = students.map(function (student) {
+  // console.log(student);
+  student.role = "student";
+  return student;
+});
+
+// console.log(updatedStudents);
+// filter
+const highScore = students.filter(function (student) {
+  // first way
+  // if (student.score >= 85) {
+  //   return student; // a value thaty coerces to true
+  // }
+  // second way
+  // if (student.score >= 80) return student;
+  return student.score >= 80;
+});
+// console.log(highScore);
+// find
+const specificId = students.find(function (student) {
+  return student.id === 1;
+  // return (id = 1);
+});
+// console.log(specificId);
+
+// reduce
+const averageScore =
+  students.reduce(function (scoresTotal, student) {
+    // console.log(student);
+    // console.log(scoresTotal);
+    return scoresTotal + student.score;
+  }, 0) / students.length;
+
+// console.log(averageScore);
+
+// square bracket notation
+const subject = "math";
+const total = {};
+// total.math = "some value";
+total[subject] = " some value";
+// console.log(total);
+
+// survey challange
+const survey = students.reduce(function (survey, student) {
+  // console.log(student.favoriteSubject);
+  const favSubject = student.favoriteSubject;
+  if (survey[favSubject]) {
+    survey[favSubject] = survey[favSubject] + 1;
+  } else {
+    survey[favSubject] = 1;
+  }
+  return survey;
+}, {});
+console.log(survey);
---
+++ Js Tute/challenges/10_data.js
@@ -0,0 +1,36 @@
+const students = [
+  {
+    id: 1,
+    name: "peter",
+    score: 80,
+    favoriteSubject: "math",
+  },
+  {
+    id: 2,
+    name: "John",
+    score: 85,
+    favoriteSubject: "English",
+  },
+  {
+    id: 3,
+    name: "king",
+    score: 90,
+    favoriteSubject: "History",
+  },
+  {
+    id: 4,
+    name: "perry",
+    score: 70,
+    favoriteSubject: "Hindi",
+  },
+  {
+    id: 5,
+    name: "parker",
+    score: 95,
+    favoriteSubject: "Hindi",
+  },
+];
+
+// function sayName(name) {
+//   console.log(name);
+// }
