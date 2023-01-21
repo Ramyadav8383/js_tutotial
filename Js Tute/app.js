de88a0d3937393fe0a5ed5f128c4e61bd30092d0
Ram Narayan Yadav <yram08913@gmail.com>
2023-01-21 16:19:13 +0530

    add new files

--- Js Tute/app.js
+++ Js Tute/app.js
@@ -1080,3 +1080,58 @@
 // document.getElementById("btn").style.color = "purple";
 
 // 49_ get element by tag name
+// select the element or group of element the we want
+// decide the effect we want to apply to the selection
+
+// getElementByTagName('tagname');
+// HTMLAllCollection = array-like object
+// index, length property but not array methods (forEach)
+
+// querySelectorAll() in most cases
+// NodeList - objects are collection of nodes
+// can run forEach
+// turn them into array - spread operator [...]
+// after that can use any array proerties
+
+// const headings = document.getElementsByTagName("h2");
+// headings[0].style.color = "red";
+// console.log(headings.length);
+
+// const items = document.getElementsByTagName("li");
+// items[2].style.color = "blue";
+// // console.log(items);
+
+// const betterItems = [...items]; // spread opeartor to change html collection to a array
+
+// betterItems.forEach(function (item) {
+//   console.log(item);
+// });
+
+// console.log(items);
+// console.log(betterItems);
+
+// 50_get element by class name
+// select the element or group of element the we want
+// decide the effect we want to apply to the selection
+
+// getElementByClassName('classname');
+// HTMLCollection
+
+// const listItems = document.getElementsByClassName("special");
+// listItems[2].style.color = "blue";
+
+// console.log(listItems);
+// const newListItem = [...listItems];
+
+// newListItem.forEach(function (item) {
+//   console.log(item);
+// });
+// console.log(listItems);
+// console.log(newListItem);
+
+// 50_query selectors and query selector all
+// select the element or group of element the we want
+// decide the effect we want to apply to the selection
+
+// querySelector('any css') - select single
+// querySelectorAll('any css') - selects all
