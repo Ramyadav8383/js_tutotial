74fe3c867c8818155dd370b21794fe0137bc5d04
Ram Narayan Yadav <yram08913@gmail.com>
2023-02-02 15:06:43 +0530

    added new file

--- Js Tute/app.js
+++ Js Tute/app.js
@@ -1447,3 +1447,26 @@
 // keypress - when key is pressed
 // keydown - when key is down
 // keyup - when key is released
+
+// const nameInput = document.getElementById("name");
+
+// nameInput.addEventListener("keypress", function () {
+//   console.log("you pressed a key");
+// });
+
+// nameInput.addEventListener("keydown", function () {
+//   console.log("you pressed a key");
+// });
+
+// nameInput.addEventListener("keyup", function () {
+//   // console.log("you pressed a key");
+//   console.log(nameInput.value);
+// });
+
+// --- 67_event object in js
+// event object argument e, evt
+// info about triggered event
+// event.type
+// event.currentTarget
+// this keyword
+// preventDefault() - prevents default behaviour
